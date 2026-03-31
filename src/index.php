b8c330271239e058550611cdd8300f0d28a588d2
Lexan <alexander@helmstedt.at>
2026-03-31 21:30:15 +0200

    Added index.php file

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once __DIR__ . '/includes/session.php';
+include_once __DIR__ . '/includes/header.php';
+include_once __DIR__ . '/includes/nav.php';
+?>
+
+<!-- CONTENT -->
+
+<?php
+include_once __DIR__ . '/includes/footer.php';
+?>
